c98f0e899fcf41a34901e2e332c63c0b301afb0d
Abhishek Kumar <abhi24033c@gmail.com>
2023-09-24 18:49:22 +0530

    mobile responsive navbar completed

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+function openNav(){
+    const bars=document.getElementById("bars")
+    const x=document.getElementById("x");
+    const menu=document.getElementById("menu");
+    const logo=document.getElementById("logo");
+    bars.style.display="none";
+    x.style.display="block";
+    menu.style.top="0rem";
+    logo.style.color="black";
+}
+
+function CloseNav(){
+    const bars=document.getElementById("bars")
+    const x=document.getElementById("x");
+    const menu=document.getElementById("menu");
+    const logo=document.getElementById("logo");
+    bars.style.display="block";
+    x.style.display="none";
+    menu.style.top="-40rem";
+    logo.style.color="white";
+}
